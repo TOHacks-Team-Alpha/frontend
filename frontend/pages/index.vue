6bfd4a63cc0b79174c3567a22e6500cfd903436a
matthewwalk <matt.walker98@outlook.com>
2021-05-08 11:52:56 -0400

    Test

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <v-row justify="center" align="center">
+  <v-row justify="center" align="center" class="mx-2">
+    <v-col cols="6">
+      <v-card color="transparent">
+        <v-card-text class="text-h3 text-md-h2 white--text">
+          Start your engines. Ready. Set. Go!
+        </v-card-text>
+        <v-card-subtitle class="text-overline">
+          Providing a sustainable solution to environmental issues
+        </v-card-subtitle>
+        <v-card-actions>
+          <v-btn rounded color="primary"> Get Started </v-btn>
+          <v-btn rounded outlined color="info"> Learn More </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="6">
+      <v-parallax src="/img/carpool.png"> </v-parallax>
+    </v-col>
+
     <v-col cols="12" sm="8" md="6">
       <v-card class="px-2">
         <v-card-text class="text-h4 text-md-h2 white--text">
